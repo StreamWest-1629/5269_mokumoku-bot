edeff92317fafef617d5ac9a081ac8a2ff5ddd93
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-10-14 20:09:58 +0900

    各apiが満たすべきインターフェイスを追加

---
+++ bot/api.go
@@ -0,0 +1,47 @@
+package bot
+
+type (
+	MokuMokuRoomId int
+	MsgFlag        int
+
+	GroupRepository interface {
+		GetMokuMoku() (room MokuMoku, err error)
+		MakeMokuMoku(name string) (room MokuMoku, err error)
+		ClearMokuMoku()
+		Println(flag MsgFlag, msg string)
+	}
+
+	TextChat interface {
+		SendMessage(msg string)
+	}
+
+	VoiceChat interface {
+		MoveToHere(member Member) error
+	}
+
+	Member interface {
+		GetUsername() string
+		UserMute(mute bool)
+	}
+
+	MokuMoku interface {
+		GetMokuMokuId() MokuMokuRoomId
+		GetName() string
+		TextChat
+		VoiceChat
+	}
+)
+
+const (
+	RoomIDDisconnect MokuMokuRoomId = -iota
+	RoomIDMokuMoku
+	RoomIDOther
+)
+
+const (
+	MsgFlagEveryone MsgFlag = 1 << iota
+	MsgFlagHere
+	MsgFlagAdmin
+	MsgFlagNone    MsgFlag = 0
+	MsgFlagSendFor MsgFlag = MsgFlagEveryone | MsgFlagHere | MsgFlagAdmin
+)
