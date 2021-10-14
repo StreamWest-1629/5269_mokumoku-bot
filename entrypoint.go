d8f004548f91eaa56b8e09e1134f238d1903f40a
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-10-15 08:21:20 +0900

    Georgeを削除

--- entrypoint.go
+++ entrypoint.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"time"
 
 	"app/apis"
@@ -11,7 +9,6 @@ import (
 
 func main() {
 	defer apis.Finalize()
-	fmt.Println("Hay! George!" + strconv.Itoa(len(apis.Finalizer)))
 	for {
 		time.Sleep(time.Minute)
 	}
