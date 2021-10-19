c75a9b6741c53683086d4943d0adbae46c659b08
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-10-20 07:57:43 +0900

    終了待機を行わせる

--- app.go
+++ app.go
@@ -3,9 +3,15 @@ package main
 import (
 	"app/apis"
 	_ "app/apis/discord"
-	// _ "app/apis/heroku"
+	_ "app/apis/heroku"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	defer apis.Finalize()
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 }
