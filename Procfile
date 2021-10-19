833a531189bc63a3f1e2321f0350d57c97f76be0
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-10-20 08:20:53 +0900

    herokuを有効化

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: app
+Web: app
--- app.go
+++ app.go
@@ -3,17 +3,19 @@ package main
 import (
 	"app/apis"
 	_ "app/apis/discord"
+	"os"
+	"os/signal"
+	"syscall"
 
-	// _ "app/apis/heroku"
-	"time"
+	_ "app/apis/heroku"
 )
 
 func main() {
 	defer apis.Finalize()
-	// sc := make(chan os.Signal, 1)
-	// // signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	// <-sc
-	for {
-		time.Sleep(time.Hour)
-	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	// for {
+	// 	time.Sleep(time.Hour)
+	// }
 }
