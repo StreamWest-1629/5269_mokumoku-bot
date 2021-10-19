cbb9159cc9bb82a65a466b657e13e3ea3b1d21bd
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-10-20 07:37:43 +0900

    heroku用heartbeatを追加

---
+++ apis/heroku/heroku.go
@@ -0,0 +1,27 @@
+package heroku
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func init() {
+	port, _ := os.LookupEnv("PORT")
+
+	http.HandleFunc("/heroku/heartbeat", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("heart beat listened")
+		w.WriteHeader(200)
+	})
+
+	go http.ListenAndServe(":"+port, nil)
+	client := http.DefaultClient
+
+	for {
+		if _, err := client.Get("http://localhost:" + port + "/heroku/heartbeat"); err != nil {
+			fmt.Println("failed to send heart beat: " + err.Error())
+		}
+		time.Sleep(20 * time.Minute)
+	}
+}
--- app.go
+++ app.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"net"
-	"os"
-	"time"
-
 	"app/apis"
 	_ "app/apis/discord"
+	_ "app/apis/heroku"
 )
 
 func main() {
 	defer apis.Finalize()
-	port, _ := os.LookupEnv("PORT")
-	net.Listen("tcp", ":"+port)
-
-	for {
-		time.Sleep(time.Minute)
-	}
 }
