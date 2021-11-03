19aa91286152e216d9829e69249b7c8327df6fbd
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-11-03 13:17:07 +0900

    生成部分のバグ修正

--- resource-enc/convert-in-docker.sh
+++ resource-enc/convert-in-docker.sh
@@ -6,27 +6,35 @@ echo "converting start!"
 
 # Set path
 mkdir ../resource
-origin_dir=$(cd ../resource-origin && pwd)
-result_dir=$(cd ../resource && pwd)
+origin_dir_gen=$(cd ../resource-origin && pwd)
+result_dir_gen=$(cd ../resource && pwd)
 echo $origin_dir
 
 # Search files
-files=`find $origin_dir -type f`
+dirs=`find $origin_dir_gen -type d`
+files=`find $origin_dir_gen -type f`
+
+for origin_dir in $dirs; do
+    result_dir=${origin_dir/$origin_dir_gen/$result_dir_gen}
+    mkdir "$result_dir"
+done
+
 for origin_file in $files; do
-    rel_path=${origin_path#origin_dir}
-    result_file=${origin_file/$origin_dir/$result_dir}
+    rel_path=${origin_path#origin_dir_gen}
+    result_file=${origin_file/$origin_dir_gen/$result_dir_gen}
     origin_file_ext=${origin_file##*.}
     result_file_noext=${result_file%.*}
 
     case "$origin_file_ext" in
     "json"|"jpg"|"png" )
         echo "copying file($rel_path)..."
-        cp origin_file result_file
+        cp "$origin_file" "$result_file"
         ;;
-    "mp3"|"wav"|"org" )
+    "mp3"|"wav"|"org"|"flac" )
         echo "converting to 'dca' file..."
-        result_file=${result_file_noext}.dca
-        ffmpeg -i $origin_file -f s16le -ar 48000 -ac 2 pipe:1 | dca > test.dca
+        result_file_dca="${result_file_noext}.dca"
+        ffmpeg -i "${origin_file}" -f s16le -ar 48000 -ac 2 pipe:1 | dca > "${result_file_dca}"
+        echo "converted: $result_file_dca ($result_file, $result_file_noext)"
         ;;
     esac
 done
